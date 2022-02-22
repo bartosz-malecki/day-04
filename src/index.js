4e38a58a69b28874b107940b072555273ade24d4
Bartosz Małecki <bartoszmalecki90@gmail.com>
2022-02-22 13:38:09 +0100

    initial commit day-04

--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// index.js
+import React from "react";
+import ReactDOM from "react-dom";
+import htmlImage from "./images/html_logo.png";
+import cssImage from "./images/css_logo.png";
+import jsImage from "./images/js_logo.png";
+import reactImage from "./images/react_logo.png";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+//ex2.1
+
+// const main = "Front End Technologies";
+// const imagesAr = [htmlImage, cssImage, jsImage, reactImage];
+
+// const header = (
+//   <header>
+//     <div className="header-wrapper">
+//       <h2>{main}</h2>
+//     </div>
+//     <div className="img-wrapper">
+//       {imagesAr.map((img, index) => (
+//         <img src={img} key={index} alt={`${img}`} />
+//       ))}
+//     </div>
+//   </header>
+// );
+// // JSX element, app
+// const app = <div className="app">{header}</div>;
+
+// ex2.2
+
+const title = "SUBSCRIBE";
+const info = "Sign up with your email address to receive news and updates";
+
+const main = (
+  <main>
+    <div className="header-wrapper">
+      <h1>{title}</h1>
+      <p>{info}</p>
+    </div>
+    <div className="form-wrapper">
+      <input placeholder="First name"></input>
+      <input placeholder="Last name"></input>
+      <input placeholder="Email"></input>
+      <button>Subscribe</button>
+    </div>
+  </main>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// JSX element, app
+const app = <div className="app">{main}</div>;
+
+const rootElement = document.getElementById("root");
+// we render the JSX element using the ReactDOM package
+ReactDOM.render(app, rootElement);
